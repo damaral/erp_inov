364a3aa85a0a47c6126039bc173f827450e2e507
Daniel A. Sousa <danpcnux@gmail.com>
2013-06-22 18:22:37 -0300

    sem storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,12 +66,12 @@ ErpInov::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.time_zone = 'Brasilia'
 
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+  #config.paperclip_defaults = {
+  #  :storage => :s3,
+  #  :s3_credentials => {
+  #    :bucket => ENV['AWS_BUCKET'],
+  #    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  #    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  #  }
+  #}
 end
